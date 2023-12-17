7517d58b1d9d108b491652d33fd9d19e73423820
Shun2439 <shunta.kobayashi24@gmail.com>
2023-12-17 18:56:48 +0900

    :white_check_mark:

---
+++ make.bat
@@ -0,0 +1,6 @@
+@echo off
+
+ptex2pdf -l -interaction batchmode question.tex
+ptex2pdf -l -interaction batchmode question.tex
+ptex2pdf -l -interaction batchmode ans.tex
+ptex2pdf -l -interaction batchmode ans.tex
