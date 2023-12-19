65436039fb50eb6ec889e4dd01ab30b84a80fa4a
Shun2439 <shunta.kobayashi24@gmail.com>
2023-12-20 08:50:44 +0900

    :fire:

--- make.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-ptex2pdf -l -interaction batchmode question.tex
-ptex2pdf -l -interaction batchmode question.tex
-ptex2pdf -l -interaction batchmode ans.tex
-ptex2pdf -l -interaction batchmode ans.tex
